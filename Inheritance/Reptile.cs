5b935baff4aeea6bc87796c8af2b02352e4be2e3
Bryan Whitman <BW.Hitman89@gmail.com>
2020-09-28 19:52:22 -0500

    ,added class Reptile

---
+++ Inheritance/Reptile.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    class Reptile : Animal
+    {
+        public bool IsColdBlooded { get; set; }
+
+        public bool ShedsSkin { get; set; }
+
+        public string Habitat { get; set; }
+
+        public bool LaysEggs { get; set; }
+
+        public Reptile()
+        {
+
+
+        }
+
+        public Reptile(string species, bool hasVertebrate, bool isCarnivore, int Legs, bool isColdBlooded, bool shedsSkin, string habitat, bool laysEggs)
+        {
+            Species = species;
+            HasVertebrate = hasVertebrate;
+            IsCarnivore = isCarnivore;
+            IsColdBlooded = isColdBlooded;
+            ShedsSkin = shedsSkin;
+            Habitat = habitat;
+            LaysEggs = laysEggs;
+
+        }
+          
+
+
+    }
+
+}
